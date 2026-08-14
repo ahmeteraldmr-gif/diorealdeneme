1b846f190148e2bce25e3ee996c3fc8bc791e21d
ahmeteraldmr-gif <ahmeteraldmr-gif@users.noreply.github.com>
2026-08-14 14:52:02 +0300

    Mirror 1-to-1 Oteller page (https://dioreal.com/oteller) texts, hotel cards 01-06, images and TR/EN options with clean Blade spans

--- resources/views/oteller.blade.php
+++ resources/views/oteller.blade.php
@@ -307,14 +307,14 @@
             </a>
         </div>
         <ul class="nav-links">
-            <li><a href="{{ route('hakkimizda') }}" data-i18n="nav_about">Hakkımızda</a></li>
-            <li><a href="{{ route('oteller') }}" class="active-page" data-i18n="nav_hotels">Oteller</a></li>
-            <li><a href="{{ route('yatlar') }}" data-i18n="nav_yachts">Yatlar</a></li>
-            <li><a href="{{ route('restoranlar') }}" data-i18n="nav_restaurants">Restoranlar</a></li>
-            <li><a href="{{ route('urunler') }}" data-i18n="nav_products">Ürünler</a></li>
-            <li><a href="{{ route('gezi-rehberi') }}" data-i18n="nav_guide">Gezi Rehberi</a></li>
-            <li><a href="{{ route('etkinlikler') }}" data-i18n="nav_events">Etkinlikler</a></li>
-            <li><a href="{{ route('journal') }}" data-i18n="nav_journal">Journal</a></li>
+            <li><a href="{{ route('hakkimizda') }}"><span class="lang-text-tr">Hakkımızda</span><span class="lang-text-en">About Us</span></a></li>
+            <li><a href="{{ route('oteller') }}" class="active-page"><span class="lang-text-tr">Oteller</span><span class="lang-text-en">Hotels</span></a></li>
+            <li><a href="{{ route('yatlar') }}"><span class="lang-text-tr">Yatlar</span><span class="lang-text-en">Yachts</span></a></li>
+            <li><a href="{{ route('restoranlar') }}"><span class="lang-text-tr">Restoranlar</span><span class="lang-text-en">Restaurants</span></a></li>
+            <li><a href="{{ route('urunler') }}"><span class="lang-text-tr">Ürünler</span><span class="lang-text-en">Products</span></a></li>
+            <li><a href="{{ route('gezi-rehberi') }}"><span class="lang-text-tr">Gezi Rehberi</span><span class="lang-text-en">Travel Guide</span></a></li>
+            <li><a href="{{ route('etkinlikler') }}"><span class="lang-text-tr">Etkinlikler</span><span class="lang-text-en">Events</span></a></li>
+            <li><a href="{{ route('journal') }}"><span class="lang-text-tr">Journal</span><span class="lang-text-en">Journal</span></a></li>
         </ul>
         <div class="nav-right" style="display: flex; align-items: center; gap: 1.5rem;">
             <a href="{{ route('sepet') }}" class="cart-nav-link" title="Sepetiniz">
@@ -329,7 +329,7 @@
             </a>
             <div class="lang-switch desk-lang">
 
-                <span id="lang-tr" class="lang-btn">TR</span>
+                <span id="lang-tr" class="lang-btn active">TR</span>
                 <span>|</span>
                 <span id="lang-en" class="lang-btn">EN</span>
             </div>
@@ -342,18 +342,18 @@
     <!-- Fullscreen Menu -->
     <div class="fs-menu" id="fsMenu">
         <ul class="fs-links">
-            <li><a href="{{ route('hakkimizda') }}" data-i18n="nav_about">Hakkımızda</a></li>
-            <li><a href="{{ route('oteller') }}" data-i18n="nav_hotels">Oteller</a></li>
-            <li><a href="{{ route('yatlar') }}" data-i18n="nav_yachts">Yatlar</a></li>
-            <li><a href="{{ route('restoranlar') }}" data-i18n="nav_restaurants">Restoranlar</a></li>
-            <li><a href="{{ route('urunler') }}" data-i18n="nav_products">Ürünler</a></li>
+            <li><a href="{{ route('hakkimizda') }}"><span class="lang-text-tr">Hakkımızda</span><span class="lang-text-en">About Us</span></a></li>
+            <li><a href="{{ route('oteller') }}"><span class="lang-text-tr">Oteller</span><span class="lang-text-en">Hotels</span></a></li>
+            <li><a href="{{ route('yatlar') }}"><span class="lang-text-tr">Yatlar</span><span class="lang-text-en">Yachts</span></a></li>
+            <li><a href="{{ route('restoranlar') }}"><span class="lang-text-tr">Restoranlar</span><span class="lang-text-en">Restaurants</span></a></li>
+            <li><a href="{{ route('urunler') }}"><span class="lang-text-tr">Ürünler</span><span class="lang-text-en">Products</span></a></li>
             <div class="fs-divider"></div>
-            <li><a href="{{ route('gezi-rehberi') }}" data-i18n="nav_guide">Gezi Rehberi</a></li>
-            <li><a href="{{ route('etkinlikler') }}" data-i18n="nav_events">Etkinlikler</a></li>
+            <li><a href="{{ route('gezi-rehberi') }}"><span class="lang-text-tr">Gezi Rehberi</span><span class="lang-text-en">Travel Guide</span></a></li>
+            <li><a href="{{ route('etkinlikler') }}"><span class="lang-text-tr">Etkinlikler</span><span class="lang-text-en">Events</span></a></li>
 
-            <li><a href="{{ route('journal') }}" data-i18n="nav_journal">Journal</a></li>
+            <li><a href="{{ route('journal') }}"><span class="lang-text-tr">Journal</span><span class="lang-text-en">Journal</span></a></li>
             <li class="lang-switch" style="font-size: 1.5rem; font-family: var(--font-display); justify-content: center; margin-top:3rem;">
-                <span id="lang-tr-fs" class="lang-btn">TR</span> | <span id="lang-en-fs" class="lang-btn">EN</span>
+                <span id="lang-tr-fs" class="lang-btn active">TR</span> | <span id="lang-en-fs" class="lang-btn">EN</span>
             </li>
         </ul>
     </div>
