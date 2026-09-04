66f6e573ce75a39dd3ed32d65497fc499b8648a8
ahmeteraldmr-gif <ahmeteraldmr-gif@users.noreply.github.com>
2026-09-04 14:35:37 +0300

    Explicitly register dioreal:sync command in routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('dioreal:sync', function () {
+    $cmd = new \App\Console\Commands\SyncDiorealContent();
+    $cmd->setOutput($this->output);
+    return $cmd->handle();
+})->purpose('Automatically scrape and sync all new content, text and images from live dioreal.com');
